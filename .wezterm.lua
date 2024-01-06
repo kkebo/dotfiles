6d7c9ddf537cef853f9a60eac7103e718fb3e8dc
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-01-07 04:15:43 +0900

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font 'SFMono Nerd Font'
+config.font_size = 12
+config.color_scheme = 'MaterialDarker'
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+return config

08575a1b6fdba81ef5a24ab1b5c376b1b0e578e0
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-04-07 03:04:05 +0900

    feat(git): add global gitignore to a-Shell too

--- install.ashell.sh
+++ install.ashell.sh
@@ -8,4 +8,6 @@ ln -s "$BASEDIR/.vim" "$HOME/Documents/"
 ln -s "$BASEDIR/.profile.ashell" "$HOME/Documents/.profile"
 ln -s "$BASEDIR/.bashrc.ashell" "$HOME/Documents/.bashrc"
 
+mkdir -p "$HOME/Documents/.config"
+ln -s "$BASEDIR/.config/git" "$HOME/Documents/.config/"
 ln -s "$BASEDIR/.gitconfig" "$HOME/Documents/"
